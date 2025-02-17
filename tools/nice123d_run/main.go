33d59b7b7014d4c083c6cae5b1549c5c12b14cd5
jdegenstein <jdegenstein@users.noreply.github.com>
2025-02-17 11:00:35 -0600

    Update main.go

--- tools/nice123d_run/main.go
+++ tools/nice123d_run/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    cmd := exec.Command("./python/python.exe", "-m", "cadviewer")
+    cmd := exec.Command("./python/python.exe", "-m", "nice123d")
 
     // Redirect the command's standard output and standard error to the Go program's standard output and standard error
     cmd.Stdout = os.Stdout
