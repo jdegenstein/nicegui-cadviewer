b3efc2d3bd2d140a379532020993ab076a0a08ac
42sol-eu <felix@42sol.eu>
2025-02-20 21:52:09 +0100

    merged

--- tools/nice123d_run/main.go
+++ tools/nice123d_run/main.go
@@ -2,20 +2,14 @@ package main
 
 import (
     "log"
-    "os"
     "os/exec"
 )
 
 func main() {
-    cmd := exec.Command("./python/python.exe", "-m", "nice123d")
-
-    // Redirect the command's standard output and standard error to the Go program's standard output and standard error
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    // Run the command
-    err := cmd.Run()
+    cmd := exec.Command("./python/python.exe", "-m", "cadviewer")
+    output, err := cmd.CombinedOutput()
     if err != nil {
         log.Fatalf("Command execution failed: %v", err)
     }
+    log.Printf("Command output: %s", output)
 }
